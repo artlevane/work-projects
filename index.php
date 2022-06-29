ddad521b79c6c4b0d4b4f15ff09b27be9d76a6d5
Levane <alevane.pfu@fujitsu.com>
2022-06-28 23:56:41 -0400

    quick email buttons v1

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+  <title>Email Buttons</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.1.1/css/fontawesome.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+    <div class="container">
+<br><br>
+    <!-- General Emails -->
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="display-3 text-center">
+          <h1>General</h1>
+        </div>
+        <div class="text-center">
+          <a href="mailto:?subject=ScanSnap Remote Support&body=Hi,%0D%0A %0D%0Ahttps://myhelpscreen.com%0D%0A%0D%0A"> 
+            <img src="https://images.squarespace-cdn.com/content/v1/57ed9839ebbd1ad3d613fb5c/1585852216820-X5Y2UQZAV3MQZCFJ39NQ/screenmeet-logo.png" class="img-thumbnail" style="width:30%; height: auto;">
+          </a>        
+        </div>
+      </div>
+    </div>
+    <br><br>
+      <h1 style="background-color: lightblue;">Software & Drivers</h1>
+      <div class="row">
+        <div class="col-sm-6">
+        <h3>ScanSnap for Windows</h3>
+        <a href="mailto:?subject=ScanSnap Home Download&body=Hi,%0D%0A %0D%0AHere is the link to the latest ScanSnap Home download: %0D%0Ahttps://www.pfu.fujitsu.com/imaging/ssacc/en/start_download_21_GA.html%0D%0A%0D%0A"><button type="button" class="btn btn-primary btn-lg">ScanSnap Home v2.6.1</button></a>
+        &nbsp;
+        <a href="mailto:?subject=ScanSnap Manager Download&body=Hi,%0D%0A %0D%0AHere is the link to the latest ScanSnap Manager download: %0D%0Ahttps://scansnap.fujitsu.com/global/dl/setup/w-ix1600-inst2-v7.html?MODEL=5020%0D%0A%0D%0A"><button type="button" class="btn btn-primary btn-lg">ScanSnap Manager v7.2</button></a>
+        <br><br>
+        <h3>ScanSnap for Mac</h3>
+        <a href="mailto:?subject=ScanSnap Home Download&body=Hi,%0D%0A %0D%0AHere is the link to the latest ScanSnap Home download: %0D%0Ahttps://www.pfu.fujitsu.com/imaging/ssacc/en/start_download_21_GA.html%0D%0A%0D%0A"><button type="button" class="btn btn-primary btn-lg">ScanSnap Home v2.6.1</button></a>
+        &nbsp;
+        <a href="mailto:?subject=ScanSnap Manager Download&body=Hi,%0D%0A %0D%0AHere is the link to the latest ScanSnap Manager download: %0D%0Ahttps://scansnap.fujitsu.com/global/dl/setup/m-ix1600-inst2-v7.html?MODEL=5020%0D%0A%0D%0A"><button type="button" class="btn btn-primary btn-lg">ScanSnap Manager v7.2</button></a>
+        </div>
+        <div class="col-sm-6">
+        <h3>fi-Series Drivers</h3>
+        <a href="mailto:?subject=Fujitsu TWAIN Driver&body=Hi,%0D%0A %0D%0AHere is the link to the latest TWAIN driver download: %0D%0Ahttps://imagescanner.fujitsu.com/global/dl/setup/psip-twain32-3100.html?MODEL=5153"><button type="button" class="btn btn-primary btn-lg">PaperStream IP (TWAIN) 3.10.0</button></a>
+        <br><br>
+        <a href="mailto:?subject=Fujitsu ISIS Driver&body=Hi,%0D%0A %0D%0AHere is the link to the latest ISIS driver download: %0D%0Ahttps://imagescanner.fujitsu.com/global/dl/setup/psip-isis-8x-3100.html?MODEL=5153%0D%0A%0D%0A"><button type="button" class="btn btn-primary btn-lg">PaperStream IP (ISIS) 3.10.0</button></a>
+        <br><br>
+        <h3>fi-Series Software</h3>
+        <a href="mailto:?subject=Fujitsu PaperStream ClickScan Download&body=Hi,%0D%0A %0D%0AHere is the link to the latest download: %0D%0Ahttps://imagescanner.fujitsu.com/global/dl/setup/psss-105.html?MODEL=5153%0D%0A%0D%0A"><button type="button" class="btn btn-primary btn-lg">PaperStream ClickScan v1.0.5</button></a>
+        <br><br>
+        <a href="mailto:?subject=Fujitsu Scanners Drivers & Downloads&body=Hi,%0D%0A %0D%0Ahttps://imagescanner.fujitsu.com/global/dl/%0D%0A%0D%0A"><button type="button" class="btn btn-primary btn-lg">General fi-Series Downloads Page</button></a>
+        </div>
+      </div>
+    </div>
+
+  </body>
+</html>
